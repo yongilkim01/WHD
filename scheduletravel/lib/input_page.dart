791671ac602a9f765cc5fa27463761868bf2c534
hanhyeonuk <wjdanfdl11@naver.com>
2023-11-30 14:26:36 +0900

    중요도표시, 버튼색상, 각슬라이더탭제목강조, 슬라이더항목추가

--- scheduletravel/lib/input_page.dart
+++ scheduletravel/lib/input_page.dart
@@ -1,7 +1,17 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'output_page.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -10,29 +20,42 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController textController1 = TextEditingController();
   DateTime startDate = DateTime.now();
   DateTime endDate = DateTime.now();
+  int selectedMaxPlaces = 1;
   final TextEditingController textController3 = TextEditingController();
   final TextEditingController textController4 = TextEditingController();
-
-  double costSliderValue = 0.3; // 비용 슬라이더의 기본 값
-  double prioritySliderValue = 0.3; // 우선항목 슬라이더의 기본 값
-  double foodSliderValue = 0.4; // 먹거리 슬라이더의 기본 값
+  double prioritySliderValue = 0.25; // 우선항목 슬라이더의 기본 값
+  double foodSliderValue = 0.25; // 먹거리 슬라이더의 기본 값
+  double shoppingSliderValue = 0.25;
+  double restSliderValue = 0.25; // 쇼핑 슬라이더의 기본 값
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('여행 정보 입력'),
+        title: const Text(
+          '여행 정보 입력',
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFC5DDFF),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextField(
                 controller: textController1,
-                decoration: const InputDecoration(labelText: '장소'),
+                decoration: InputDecoration(
+                  labelText: '장소',
+                  contentPadding: const EdgeInsets.all(16.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
+              const SizedBox(height: 16.0),
               Row(
                 children: [
                   Expanded(
@@ -41,30 +64,40 @@ class _HomePageState extends State<HomePage> {
                         _selectDate(context, isStartDate: true);
                       },
                       child: InputDecorator(
-                        decoration: const InputDecoration(
+                        decoration: InputDecoration(
                           labelText: '출발 날짜',
+                          contentPadding: const EdgeInsets.all(16.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
                         ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
                             Text(
                               "${startDate.toLocal()}".split(' ')[0],
+                              style: const TextStyle(fontSize: 16.0),
                             ),
-                            Icon(Icons.calendar_today),
+                            const Icon(Icons.calendar_today, size: 20.0),
                           ],
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(width: 8.0),
+                  const SizedBox(width: 8.0),
                   Expanded(
                     child: InkWell(
                       onTap: () {
                         _selectDate(context, isStartDate: false);
                       },
                       child: InputDecorator(
-                        decoration: const InputDecoration(
+
+                        decoration: InputDecoration(
                           labelText: '복귀 날짜',
+                          contentPadding: const EdgeInsets.all(16.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
                         ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -72,7 +105,7 @@ class _HomePageState extends State<HomePage> {
                             Text(
                               "${endDate.toLocal()}".split(' ')[0],
                             ),
-                            Icon(Icons.calendar_today),
+                            const Icon(Icons.calendar_today),
                           ],
                         ),
                       ),
@@ -80,89 +113,196 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+              const SizedBox(height: 16.0),
+              DropdownButtonFormField<int>(
+                value: selectedMaxPlaces,
+                onChanged: (value) {
+                  setState(() {
+                    selectedMaxPlaces = value!;
+                  });
+                },
+                items: List.generate(5, (index) {
+                  return DropdownMenuItem<int>(
+                    value: index + 1,
+                    child: Text('${index + 1}'),
+                  );
+                }),
+                decoration: InputDecoration(
+                  labelText: '일 최대 방문 장소 수',
+                  contentPadding: const EdgeInsets.all(16.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16.0),
               TextField(
                 controller: textController3,
-                decoration: const InputDecoration(labelText: '비용'),
+                decoration: InputDecoration(
+                  labelText: '비용',
+                  contentPadding: const EdgeInsets.all(16.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
-              // 비용 슬라이더
+              const SizedBox(height: 8.0),
+              const Text(
+                '휴양',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                  color: Colors.black, // Customize the color
+                ),
+              ),
               Column(
                 children: [
-                  Text('비용: ${costSliderValue.toStringAsFixed(1)}'),
-                  Slider(
-                    value: costSliderValue,
-                    onChanged: (value) {
-                      setState(() {
-                        costSliderValue = _clampValue(value);
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('(낮음)'),
+                      Expanded(
+                        child: Slider(
+                          value: restSliderValue,
+                          onChanged: (value) {
+                            setState(() {
+                              restSliderValue = _clampValue(value);
+                              _adjustWeights();
+                            });
+                          },
+                        ),
+                      ),
+                      const Text('(높음)'),
+                    ],
                   ),
                 ],
               ),
-              TextField(
-                controller: textController4,
-                decoration: const InputDecoration(labelText: '관광'),
+              const SizedBox(height: 8.0),
+              const Text(
+                '관광',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                  color: Colors.black, // Customize the color
+                ),
               ),
               // 우선항목 슬라이더
               Column(
                 children: [
-                  Text('관광: ${prioritySliderValue.toStringAsFixed(1)}'),
-                  Slider(
-                    value: prioritySliderValue,
-                    onChanged: (value) {
-                      setState(() {
-                        prioritySliderValue = _clampValue(value);
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('(낮음)'),
+                      Expanded(
+                        child: Slider(
+                          value: prioritySliderValue,
+                          onChanged: (value) {
+                            setState(() {
+                              prioritySliderValue = _clampValue(value);
+                              _adjustWeights();
+                            });
+                          },
+                        ),
+                      ),
+                      const Text('(높음)'),
+                    ],
                   ),
                 ],
               ),
+              const SizedBox(height: 8.0),
               // 먹거리 슬라이더
-              TextField(
-                decoration: const InputDecoration(labelText: '먹거리'),
+              const Text(
+                '먹거리',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                  color: Colors.black, // Customize the color
+                ),
               ),
               Column(
                 children: [
-                  Text('먹거리: ${foodSliderValue.toStringAsFixed(1)}'),
-                  Slider(
-                    value: foodSliderValue,
-                    onChanged: (value) {
-                      setState(() {
-                        foodSliderValue = _clampValue(value);
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('(낮음)'),
+                      Expanded(
+                        child: Slider(
+                          value: foodSliderValue,
+                          onChanged: (value) {
+                            setState(() {
+                              foodSliderValue = _clampValue(value);
+                              _adjustWeights();
+                            });
+                          },
+                        ),
+                      ),
+                      const Text('(높음)'),
+                    ],
                   ),
                 ],
               ),
-              SizedBox(height: 16.0),
+              const SizedBox(height: 8.0),
+              const Text(
+                '쇼핑',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                  color: Colors.black, // Customize the color
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text('(낮음)'),
+                      Expanded(
+                        child: Slider(
+                          value: shoppingSliderValue,
+                          onChanged: (value) {
+                            setState(() {
+                              shoppingSliderValue = _clampValue(value);
+                              _adjustWeights();
+                            });
+                          },
+                        ),
+                      ),
+                      const Text('(높음)'),
+                    ],
+                  ),
+                ],
+              ),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => OutputPage(
-                        text1: textController1.text,
-                        text2: "시작: ${startDate.toLocal()} 종료: ${endDate.toLocal()}",
-                        text3: textController3.text,
-                        text4: textController4.text,
-                        costSliderValue: costSliderValue,
-                        prioritySliderValue: prioritySliderValue,
-                        foodSliderValue: foodSliderValue,
+                          text1: textController1.text,
+                          text2: "시작: ${startDate.toLocal()} 종료: ${endDate.toLocal()}",
+                          text3: textController3.text,
+                          selectedMaxPlaces: selectedMaxPlaces,
+                          prioritySliderValue: prioritySliderValue,
+                          foodSliderValue: foodSliderValue,
+                          shoppingSliderValue : shoppingSliderValue,
+                          restSliderValue: restSliderValue
                       ),
                     ),
                   );
                 },
-                child: const Text('여행 추천'),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ), // 새로운 테마 컬러
+                  backgroundColor: const Color(0xFFB3E5FC),
+                ),
+                child: const Text(
+                  '여행 추천',
+                  style: TextStyle(fontSize: 18.0),
+                ),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  double _clampValue(double value) {
-    // Ensure the value is between 0 and 1
-    return value.clamp(0.0, 1.0);
   }
 
   Future<void> _selectDate(BuildContext context, {required bool isStartDate}) async {
@@ -180,6 +320,33 @@ class _HomePageState extends State<HomePage> {
           endDate = picked;
         }
       });
+    }
+  }
+
+  double _clampValue(double value) {
+    const double minValue = 0.0;
+    const double maxValue = 1.0;
+
+    return value.clamp(minValue, maxValue);
+  }
+
+  void _adjustWeights() {
+    const double totalWeight = 1.0;
+
+    double currentPriorityWeight = prioritySliderValue;
+    double currentFoodWeight = foodSliderValue;
+    double currentShoppingWeight = shoppingSliderValue;
+    double currentRestWeight = restSliderValue;
+
+    // 현재 가중치의 합
+    double currentTotalWeight = currentPriorityWeight + currentFoodWeight + currentShoppingWeight +currentRestWeight;
+
+    // 합이 0이 아니면 각 슬라이더의 값을 조절하여 합이 1이 되도록 함
+    if (currentTotalWeight != 0) {
+      prioritySliderValue = (currentPriorityWeight / currentTotalWeight) * totalWeight;
+      foodSliderValue = (currentFoodWeight / currentTotalWeight) * totalWeight;
+      shoppingSliderValue = (currentShoppingWeight / currentTotalWeight) * totalWeight;
+      restSliderValue = (currentRestWeight / currentTotalWeight) * totalWeight;
     }
   }
 }
